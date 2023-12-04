d8b20c273a80774e8e3fc5966098d5ccfaf498d0
DavideDalDosso <DavideDalDosso08@gmail.com>
2023-12-04 15:19:53 +0100

    Setup

---
+++ Rush00/AdventureEngine.cs
@@ -0,0 +1,27 @@
+﻿using Rush00;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class AdventureEngine
+{
+    private List<Room> rooms = new List<Room>();
+    private Room currentRoom;
+
+    public void InputPrompt()
+    {
+
+    }
+
+    public void OutputRoomDesc()
+    {
+
+    }
+
+    public void ChangeRoom(int exitIndex)
+    {
+
+    }
+}
